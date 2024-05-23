db7025ba02d7cc2434c1c49871bf2207a93a0418
Garnet Grimm <garnetgrimm720@gmail.com>
2024-05-23 06:35:14 -0600

    autformat

--- ExampleProject.cpp
+++ ExampleProject.cpp
@@ -7,24 +7,25 @@
 using namespace daisy;
 
 // Declare a DaisySeed object called hardware
-DaisySeed hardware;
+DaisySeed      hardware;
 MidiUsbHandler midi;
-Adsr adsr; //TODO: explore daisysp adsr
-uint16_t pitch;
+Adsr           adsr; //TODO: explore daisysp adsr
+uint16_t       pitch;
 
-static constexpr size_t dac_buf_len = 48;
+static constexpr size_t         dac_buf_len = 48;
 uint16_t DMA_BUFFER_MEM_SECTION dac_buf[2][dac_buf_len];
 
 void AudioCallback(AudioHandle::InputBuffer  in,
                    AudioHandle::OutputBuffer out,
                    size_t                    size)
 {
-
 }
 
-void my_dac_callback(uint16_t **out, size_t size) {
-	uint16_t adsr_value = adsr.step() * 0xFFF;
-    for(size_t i = 0; i < size; i++) {
+void my_dac_callback(uint16_t **out, size_t size)
+{
+    uint16_t adsr_value = adsr.step() * 0xFFF;
+    for(size_t i = 0; i < size; i++)
+    {
         out[0][i] = pitch;
         out[1][i] = adsr_value;
     }
@@ -35,18 +36,18 @@ int main(void)
     hardware.Configure();
     hardware.Init();
 
-	MidiUsbHandler::Config midi_cfg;
+    MidiUsbHandler::Config midi_cfg;
     midi_cfg.transport_config.periph = MidiUsbTransport::Config::INTERNAL;
     midi.Init(midi_cfg);
 
-	DacHandle::Config dac_cfg;
-	dac_cfg.target_samplerate = 48000; //16kHz
-	dac_cfg.bitdepth   = DacHandle::BitDepth::BITS_12;
-	dac_cfg.buff_state = DacHandle::BufferState::ENABLED;
-	dac_cfg.mode       = DacHandle::Mode::DMA;
-	dac_cfg.chn        = DacHandle::Channel::BOTH;
-	hardware.dac.Init(dac_cfg);
-	hardware.dac.Start(dac_buf[0], dac_buf[1], dac_buf_len, my_dac_callback);
+    DacHandle::Config dac_cfg;
+    dac_cfg.target_samplerate = 48000; //16kHz
+    dac_cfg.bitdepth          = DacHandle::BitDepth::BITS_12;
+    dac_cfg.buff_state        = DacHandle::BufferState::ENABLED;
+    dac_cfg.mode              = DacHandle::Mode::DMA;
+    dac_cfg.chn               = DacHandle::Channel::BOTH;
+    hardware.dac.Init(dac_cfg);
+    hardware.dac.Start(dac_buf[0], dac_buf[1], dac_buf_len, my_dac_callback);
 
     AdcChannelConfig adc_cfg[4];
     adc_cfg[0].InitSingle(hardware.GetPin(18));
@@ -55,11 +56,12 @@ int main(void)
     adc_cfg[3].InitSingle(hardware.GetPin(24));
     hardware.adc.Init(&adc_cfg[0], 4);
 
-	Led led[8];
-	for(int i = 0; i < 8; i++) {
-		led[i].Init(hardware.GetPin(i+7), false);
-		led[i].Set(0.0f);
-	}
+    Led led[8];
+    for(int i = 0; i < 8; i++)
+    {
+        led[i].Init(hardware.GetPin(i + 7), false);
+        led[i].Set(0.0f);
+    }
 
     hardware.adc.Start();
 
@@ -73,20 +75,21 @@ int main(void)
             {
                 case NoteOn:
                 {
-					auto note_msg = msg.AsNoteOn();
-					if(note_msg.velocity != 0) {
-						adsr.params.attack_time = hardware.adc.GetFloat(0);
-						adsr.params.decay_time = hardware.adc.GetFloat(1);
-						adsr.params.sustain_amt = hardware.adc.GetFloat(2);
-						adsr.params.release_time = hardware.adc.GetFloat(3);
-						adsr.trigger();
-						float freq = daisysp::mtof(note_msg.note);
-						float L = 220;
-						float H = 880;
-						float m = 1.0f/(H-L);
-						float b = 1.0f/((H/L)-1.0f);
-						pitch = ((m*freq + b) * 2.0f) * 0xFFF;
-					}
+                    auto note_msg = msg.AsNoteOn();
+                    if(note_msg.velocity != 0)
+                    {
+                        adsr.params.attack_time  = hardware.adc.GetFloat(0);
+                        adsr.params.decay_time   = hardware.adc.GetFloat(1);
+                        adsr.params.sustain_amt  = hardware.adc.GetFloat(2);
+                        adsr.params.release_time = hardware.adc.GetFloat(3);
+                        adsr.trigger();
+                        float freq = daisysp::mtof(note_msg.note);
+                        float L    = 220;
+                        float H    = 880;
+                        float m    = 1.0f / (H - L);
+                        float b    = 1.0f / ((H / L) - 1.0f);
+                        pitch      = ((m * freq + b) * 2.0f) * 0xFFF;
+                    }
                 }
                 break;
                 default: break;
